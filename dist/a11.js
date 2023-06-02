dc840280d28f4569dc624f741c40af572fc6d2e2
devaspirant0510 <seungho020510@gmail.com>
2023-06-02 18:41:04 +0900

    lit element example code

---
+++ dist/a11.js
@@ -0,0 +1,10 @@
+
+import {LitElement,html} from "lit";
+
+export class aaa extends LitElement{
+    render(){
+        return html`<h1>LitTempleate</h1>`
+    }
+
+}
+customElements.define("aaa",aaa)
