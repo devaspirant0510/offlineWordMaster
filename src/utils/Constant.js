7c0d97296681f1e628d5c0c82531dbdcc8fa12d7
devaspirant0510 <seungho020510@gmail.com>
2023-06-11 18:34:58 +0900

    feat : css reference 상수값

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -2,3 +2,12 @@ export const ViewState = {
     VISIBLE:"visible",
     HIDDEN:"hidden"
 }
+
+export const CSS_REF = {
+    WORD_LIST_CTX           : "word-ctx-menu",
+    WORD_LIST_UPDATE_BUTTON : "btn-word-delete",
+    WORD_LIST_DELETE_BUTTON : "btn-word-update",
+    WORD_ITEM_CTX           : "word-item-ctx-menu",
+    WORD_ITEM_UPDATE_BUTTON : "btn-word-item-delete",
+    WORD_ITEM_DELETE_BUTTON : "btn-word-item-update"
+}
