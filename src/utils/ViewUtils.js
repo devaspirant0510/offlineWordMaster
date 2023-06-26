2a0d1b5aff6ebae100d887ccca4f1fe1c2282fc0
devaspirant0510 <seungho020510@gmail.com>
2023-06-26 19:04:10 +0900

    feat : 뷰 display none,block 유틸 함수

---
+++ src/utils/ViewUtils.js
@@ -0,0 +1,13 @@
+/**
+ * @param {HTMLElement} element 
+ */
+export function displayNone(element){
+    element.style.display = "none"
+}
+
+/**
+ * @param {HTMLElement} element 
+ */
+export function displayShowen(element){
+    element.style.display = "block"
+}
