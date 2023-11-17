2bb939955650d6a00fc8117675b8cde475c400f4
Seung <seungho020510@gmail.com>
2023-11-17 18:35:45 +0900

    feat: vite base 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,32 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/main/App.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RepositoryImpl from './data/repository/RepositoryImpl.ts';
 import LocalDataSource from './data/datasource/LocalDataSource.ts';
 import Repository from './domain/Repository.ts';
+import {  HashRouter, Route, Routes } from 'react-router-dom';
+import App from './pages/main/App.tsx';
 
 const queryClient = new QueryClient();
 
 const ds = new LocalDataSource();
-const repo = new RepositoryImpl(ds)
+const repo = new RepositoryImpl(ds);
 export const RepoContext = createContext<Repository>(repo);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<RepoContext.Provider value={repo}>
-		<QueryClientProvider client={queryClient}>
-			<React.StrictMode>
-				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<App />} />
-					</Routes>
-				</BrowserRouter>
-			</React.StrictMode>
-		</QueryClientProvider>
+const Text = () => {
+	return (<div><h1>3</h1></div>);
+};
 
-	</RepoContext.Provider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<HashRouter>
+		<RepoContext.Provider value={repo}>
+			<QueryClientProvider client={queryClient}>
+				<Routes>
+					<Route path='/' element={<App />} />
+					<Route path='/333' element={<Text />} />
+				</Routes>
+			</QueryClientProvider>
+
+		</RepoContext.Provider>
+	</HashRouter>,
 );
