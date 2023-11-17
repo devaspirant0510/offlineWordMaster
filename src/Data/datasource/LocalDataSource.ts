035d0b72d922efcc949dc3f5d4ec1d131a031c3b
Seung <seungho020510@gmail.com>
2023-11-17 14:58:50 +0900

    feat : 배포용 빌드

--- src/Data/datasource/LocalDataSource.ts
+++ src/Data/datasource/LocalDataSource.ts
@@ -1,25 +1,27 @@
 import DBManager from './DBManager.ts';
 import DictionaryEntity from '../entity/DictionaryEntity.ts';
 
-class LocalDataSource{
-	dm:DBManager;
+class LocalDataSource {
+	dm: DBManager;
+
 	constructor() {
 		this.dm = new DBManager();
 	}
-	async loadAllDictionary(){
+
+	async loadAllDictionary() {
 		const store = await this.dm.getObjectStore();
 		return await this.dm.transactionMapper<DictionaryEntity[]>(store.getAll());
 	}
 
-	async saveDictionary(dict:DictionaryEntity){
+	async saveDictionary(dict: DictionaryEntity) {
 		const store = await this.dm.getObjectStore();
 		const addReq = store.add(dict);
 		const result = await this.dm.transactionMapper<IDBValidKey>(addReq);
-		const addRequest = store.get(result)
-		return await this.dm.transactionMapper<DictionaryEntity>(addRequest)
+		const addRequest = store.get(result);
+		return await this.dm.transactionMapper<DictionaryEntity>(addRequest);
 
 
 	}
 }
 
-export default LocalDataSource
+export default LocalDataSource;
